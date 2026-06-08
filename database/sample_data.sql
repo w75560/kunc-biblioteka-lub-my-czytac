4c3ef25b8c7aa0b8689e192e4699ce673b9e213c
Ömer Faruk Künç-75560 <omerfarukkunc66@gmail.com>
2026-06-08 18:13:49 +0200

    Dodanie przykładowych książek
    Dodanie przykładowych książek

---
+++ database/sample_data.sql
@@ -0,0 +1,26 @@
+insert into public.books (title, author, description, genre, publication_year, cover_url)
+values
+(
+    'Lalka',
+    'Bolesław Prus',
+    'Klasyczna powieść o Stanisławie Wokulskim, jego ambicjach, uczuciach i społeczeństwie Warszawy XIX wieku.',
+    'powieść',
+    1890,
+    'https://covers.openlibrary.org/b/isbn/9781784351151-L.jpg?default=false'
+),
+(
+    'Mały Książę',
+    'Antoine de Saint-Exupéry',
+    'Krótka, symboliczna opowieść o przyjaźni, odpowiedzialności i patrzeniu na świat z uważnością.',
+    'opowieść',
+    1943,
+    'https://covers.openlibrary.org/b/isbn/0547978847-L.jpg?default=false'
+),
+(
+    'Solaris',
+    'Stanisław Lem',
+    'Powieść science fiction o kontakcie człowieka z tajemniczym oceanem i o granicach ludzkiego poznania.',
+    'science fiction',
+    1961,
+    'https://covers.openlibrary.org/b/isbn/8493480304-L.jpg?default=false'
+);
